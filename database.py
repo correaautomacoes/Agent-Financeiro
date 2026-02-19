b4bc490fbc52a750169274fdc7f7d61b66a5a52b
Mateus Corrêa <correaautomacoes@gmail.com>
2026-02-18 21:23:43 -0300

    update

--- database.py
+++ database.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 load_dotenv(override=True)
 
 # Ler configurações do .env com valores padrão
-DB_HOST = os.getenv("DB_HOST", "localhost")
+DB_HOST = os.getenv("DB_HOST", "postgres") # No Docker, o host é o nome do serviço
 DB_PORT = os.getenv("DB_PORT", "5432")
 DB_NAME = os.getenv("DB_NAME", "financial_db")
 DB_USER = os.getenv("DB_USER", "admin")
